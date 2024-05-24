f75ba171b19440e36d83bc65383266a281fe83d1
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-24 20:31:01 +0700

    Add route to edit listing and seller listing

--- sell/views.py
+++ sell/views.py
@@ -6,8 +6,8 @@ from django.shortcuts import render
 def create_listing(request):
     return render(request, 'form_listing.html')
 def get_seller_listing(request):
-    pass
+    return render(request, 'seller_listing.html')
 def edit_listing(request):
-    pass
+    return render(request, 'form_listing.html')
 def get_seller_order(request):
     pass
