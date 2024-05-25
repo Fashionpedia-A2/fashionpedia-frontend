d24cd30589f7d092b3271166601926f264b6afb8
Ilham Abdillah Alhamdi <63908394+ilhamelhamdi@users.noreply.github.com>
2024-05-25 19:20:26 +0700

    feat: Add seller order page

--- sell/views.py
+++ sell/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+
 def index(request):
     return redirect('get_seller_listing')
 
+
 def create_listing(request):
     return render(request, 'form_listing.html')
+
+
 def get_seller_listing(request):
     return render(request, 'seller_listing.html')
+
+
 def edit_listing(request):
     return render(request, 'form_listing.html')
+
+
 def get_seller_order(request):
-    pass
+    return render(request, 'seller_order.html')
